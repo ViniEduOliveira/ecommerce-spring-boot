aed7d54fb6eeb1cfc926bb82da0ac9d5b4f80850
ViniEduOliveira <vinicius.edu.soliveira@gmail.com>
2026-08-14 23:38:53 -0300

    feat: add findAll and findById endpoints and validation in User entity

--- src/main/java/com/vinieduoliveira/ecommerce/model/User.java
+++ src/main/java/com/vinieduoliveira/ecommerce/model/User.java
@@ -2,21 +2,34 @@ package com.vinieduoliveira.ecommerce.model;
 
 import com.vinieduoliveira.ecommerce.common.BaseModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_user")
 public class User extends BaseModel {
 
+    @NotBlank(message = "The name can't be empty ")
+    @Size(min = 3, message = "The name needs at least 3 characters ")
     private String name;
+
+    @NotBlank(message = "The email can't be empty ")
+    @Email(message = "invalid email format")
     private String email;
+
+    @NotBlank(message = "The phone can't be empty ")
+    @Pattern(regexp = "^\\d{2}9\\d{8}$", message = "Invalid phone (use area code + 9 + number, digits only)")
     private String phone;
+
+    @NotBlank(message = "The password can't be empty ")
     private String password;
 
     public User() {
     }
 
     public User(String name, String email, String phone, String password) {
-
         this.name = name;
         this.email = email;
         this.phone = phone;
