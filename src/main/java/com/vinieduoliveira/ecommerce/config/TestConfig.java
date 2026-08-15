b405aab7d3af8175951750e598a1a029f8202aee
ViniEduOliveira <vinicius.edu.soliveira@gmail.com>
2026-08-14 23:39:10 -0300

    feat: seed test database with sample users

--- src/main/java/com/vinieduoliveira/ecommerce/config/TestConfig.java
+++ src/main/java/com/vinieduoliveira/ecommerce/config/TestConfig.java
@@ -1,4 +1,29 @@
 package com.vinieduoliveira.ecommerce.config;
 
-public class TestConfig {
+
+import com.vinieduoliveira.ecommerce.model.User;
+import com.vinieduoliveira.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User("Vinicius Eduardo", "vinicius@gmail.com", "11988888888", "123456");
+        User u2 = new User("Giulia Ayumi", "giulia@gmail.com", "11977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+
 }
